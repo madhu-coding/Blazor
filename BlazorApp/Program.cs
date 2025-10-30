15db30c4c999348bbe9a073453ce1f6e801c3c1f
Madhusudhan V <madhusudhan.v@gainwelltechnologies.com>
2025-10-30 09:08:42 +0530

    Auto Render Modes

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,3 +1,4 @@
+using BlazorApp.Client.Pages;
 using BlazorApp.Components;
 
 namespace BlazorApp
@@ -9,12 +10,18 @@ namespace BlazorApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddRazorComponents();
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -27,7 +34,10 @@ namespace BlazorApp
             app.UseAntiforgery();
 
             app.MapStaticAssets();
-            app.MapRazorComponents<App>();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
 
             app.Run();
         }
